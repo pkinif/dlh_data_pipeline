fcdb5012f1204e1aae746420396a888585284378
Pierrick Kinif <pierrick-kinif@hotmail.be>
2025-04-19 22:52:17 +0200

    Update pipeline.R

--- pipeline.R
+++ pipeline.R
@@ -1,5 +1,3 @@
-
-library(pipelineR)
 message("Testing start_pipeline() on a small batch...")
 batch_size_jenkins <- as.integer(Sys.getenv("JENKINS_BATCH_SIZE"))
 message("Batch size: ", batch_size_jenkins)
