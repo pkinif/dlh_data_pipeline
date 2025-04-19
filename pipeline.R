2d5e093871ef20589f52508c45f598dbda5c06eb
Pierrick Kinif <pierrick-kinif@hotmail.be>
2025-04-19 22:52:07 +0200

    Update pipeline.R
    remoive useless code

--- pipeline.R
+++ pipeline.R
@@ -1,20 +1,11 @@
 
 library(pipelineR)
-library(DBI)
-
 message("Testing start_pipeline() on a small batch...")
-
 batch_size_jenkins <- as.integer(Sys.getenv("JENKINS_BATCH_SIZE"))
-
 message("Batch size: ", batch_size_jenkins)
-
 pipelineR::start_pipeline(
   from = Sys.Date() - 5,
   to = Sys.Date(),
   batch_size = batch_size_jenkins
 )
-
-
 message("✅ start_pipeline() completed without crash.")
-
-DBI::dbDisconnect(con)
