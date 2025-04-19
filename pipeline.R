52d9187b7e90ade79b15d7705b359c9e653f49bd
Pierrick Kinif <pierrick-kinif@hotmail.be>
2025-04-19 19:47:38 +0200

    add message

--- pipeline.R
+++ pipeline.R
@@ -8,6 +8,8 @@ message("Testing start_pipeline() on a small batch...")
 
 batch_size_jenkins <- as.integer(sys.getenv("JENKINS_BATCH_SIZE"))
 
+message("Batch size: ", batch_size_jenkins)
+
 pipelineR::start_pipeline(
   from = Sys.Date() - 5,
   to = Sys.Date(),
